79d6e8d212561eb667311ecb9dc56e9259299dd7
mario robayo <marioferyhwh@gmail.com>
2022-04-06 11:03:10 -0500

    se realiza ejercicio 2

--- src/app/modules/numero-inverso/numero-inverso.component.ts
+++ src/app/modules/numero-inverso/numero-inverso.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-numero-inverso',
-  templateUrl: './numero-inverso.component.html',
-  styleUrls: ['./numero-inverso.component.scss']
+  selector: "app-numero-inverso",
+  templateUrl: "./numero-inverso.component.html",
+  styleUrls: ["./numero-inverso.component.scss"],
 })
 export class NumeroInversoComponent implements OnInit {
+  public invert: string = "";
 
-  public
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  calInverso(evt: any): void {
+    const base: string = evt.target.value;
+    this.invert = base.split("").reverse().join("");
   }
-
-
-
 }
